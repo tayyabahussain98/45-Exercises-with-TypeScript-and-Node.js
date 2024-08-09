06b36873d83e7fca6cd9b5492dae7f06b0b4f03e
Tayyaba Hussain <tayyabahussain1998@gmail.com>
2024-08-09 10:40:24 +0500

    Q3 commit

---
+++ Q03_Name_Cases/main.ts
@@ -0,0 +1,8 @@
+/*Q03: Name Cases: Store a person’s name in a variable, and then print that person’s name in
+lowercase, uppercase, and titlecase. */
+
+let name: string = "Tayyaba"
+
+console.log(name.toLowerCase());
+console.log(name.toUpperCase());
+console.log(name.charAt(0).toUpperCase() + name.slice(1).toLowerCase())
