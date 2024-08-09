a522ed70aba58b43830e728f4c4dfe7c72b33ece
Tayyaba Hussain <tayyabahussain1998@gmail.com>
2024-08-09 17:53:45 +0500

    Q20 commit

---
+++ Q20_Empty_Array/main.js
@@ -0,0 +1,12 @@
+/*Q20: Empty Array:Think of something you could store in a array. For example, you could make a list of
+mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program
+that creates a list containing these items. */
+//Think of something you could store in a array.
+let items = ["Pakistan", "Karachi", "Urdu"];
+//Write a program that creates a list containing these items.
+let item2 = [];
+item2.push("Germany");
+item2.push("England");
+item2.push("India");
+console.log(item2);
+export {};
---
+++ Q20_Empty_Array/main.ts
@@ -0,0 +1,17 @@
+/*Q20: Empty Array:Think of something you could store in a array. For example, you could make a list of
+mountains, rivers, countries, cities, languages, or anything else you’d like. Write a program
+that creates a list containing these items. */
+
+//Think of something you could store in a array.
+
+let items: string[] = ["Pakistan", "Karachi", "Urdu"];
+
+//Write a program that creates a list containing these items.
+
+let item2: string[] = [];
+
+item2.push("Germany");
+item2.push("England");
+item2.push("India");
+
+console.log(item2);
