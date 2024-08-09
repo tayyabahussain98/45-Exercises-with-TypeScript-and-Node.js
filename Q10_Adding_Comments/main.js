cf44280e94ab6fb0caaf2cd44ac4c53c1d7f1d38
Tayyaba Hussain <tayyabahussain1998@gmail.com>
2024-08-09 11:45:19 +0500

    Q10 commit

---
+++ Q10_Adding_Comments/main.js
@@ -0,0 +1,15 @@
+/*Q10: Adding Comments: Choose two of the programs you’ve written, and add at least one comment
+to each. If you don’t have anything specific to write because your programs are too simple at
+this point, just add your name and the current date at the top of each program file.
+Then write one sentence describing what the program does.*/
+//Tayyaba Hussain
+/*The code is below simply to addition, subtractin, multipication and division the values and
+create a same output `8`*/
+console.log(5 + 3); // Addition
+console.log(15 - 7); // Subtraction
+console.log(4 * 2); // Multiplication
+console.log(32 / 4); // Division
+//This code is below simply print to Hello World.
+let printHello = "Hello World!!!";
+console.log(printHello);
+export {};
