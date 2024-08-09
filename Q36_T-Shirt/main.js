3baf6dfbadbbdee166ac3f8e7d39439a9525d992
Tayyaba Hussain <tayyabahussain1998@gmail.com>
2024-08-09 20:23:29 +0500

    Q36 commit

---
+++ Q36_T-Shirt/main.js
@@ -0,0 +1,10 @@
+/*Q36: T-Shirt: Write a function called make_shirt() that accepts a size and the text of a
+message that should be printed on the shirt. The function should print a sentence summarizing
+the size of the shirt and the message printed on it. Call the function.*/
+function make_shirt(size, text) {
+    console.log(`Creating a ${size} shirt with the message: ${text}.`);
+}
+make_shirt("Large", "Hello World");
+make_shirt("Medium", "Hello World");
+make_shirt("Small", "Hello World");
+export {};
---
+++ Q36_T-Shirt/main.ts
@@ -0,0 +1,16 @@
+/*Q36: T-Shirt: Write a function called make_shirt() that accepts a size and the text of a
+message that should be printed on the shirt. The function should print a sentence summarizing
+the size of the shirt and the message printed on it. Call the function.*/
+
+
+function make_shirt(size: string, text: string){
+
+    console.log(`Creating a ${size} shirt with the message: ${text}.`);
+    
+}
+
+make_shirt("Large", "Hello World");
+
+make_shirt("Medium", "Hello World");
+
+make_shirt("Small", "Hello World");
