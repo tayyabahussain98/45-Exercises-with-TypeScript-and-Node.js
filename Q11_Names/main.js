87f95cb10d0aada5702b1d510eb91abdd734831e
Tayyaba Hussain <tayyabahussain1998@gmail.com>
2024-08-09 11:50:50 +0500

    Q11 commit

---
+++ Q11_Names/main.js
@@ -0,0 +1,15 @@
+/*Q11: Names: Store the names of a few of your friends in a aaray called
+names.Print each person's name by accessing each element in the list, one
+at a time. */
+let friendsName = ["Tayyaba", "Najma", "Uzma", "Kainat", "Nida", "Hira", "Misbah"];
+// console.log(friendsName[0]);
+// console.log(friendsName[1]);
+// console.log(friendsName[2]);
+// console.log(friendsName[3]);
+// console.log(friendsName[4]);
+// console.log(friendsName[5]);
+// console.log(friendsName[6]);
+for (let i = 0; i < friendsName.length; i++) {
+    console.log(friendsName[i]);
+}
+export {};
