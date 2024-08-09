631e0a6cc707d9fe6b73ad0d8a0828dabda75f97
Tayyaba Hussain <tayyabahussain1998@gmail.com>
2024-08-09 16:43:09 +0500

    Q14 commit

---
+++ Q14_Guest_List/main.ts
@@ -0,0 +1,11 @@
+/*Q14: Guest List: If you could invite anyone, living or deceased, to dinner, who would you
+invite? Make a list that includes at least three people you’d like to invite to dinner.
+Then use your list to print a message to each person, inviting them to dinner. */
+
+let guestList: string[] = ["Imran Khan", "Nawaz Sharif", "Asif Ali Zardari"];
+
+for (let i = 0; i < guestList.length; i++) {
+  console.log(
+    `\nDear Mr. ${guestList[i]}, \n\nIt's our pleasure to invite you in our party.\n\nThank you!\n\n`
+  );
+}
