1e890db1bd6961ffd7fa35db3f6e20b765db40b8
Tayyaba Hussain <tayyabahussain1998@gmail.com>
2024-08-09 10:24:38 +0500

    first commit

---
+++ Q02_Personal_Message/main.js
@@ -0,0 +1,6 @@
+/*Q02: Personal Message: Store a person’s name in a variable, and print a message to that
+person. Your message should be simple, such as, “Hello Eric, would you like to learn some
+Python today?” */
+let name = "Tayyaba";
+console.log(`Hello! ${name}, Would you like to learn some TypeScript today?`);
+export {};
---
+++ Q02_Personal_Message/main.ts
@@ -0,0 +1,7 @@
+/*Q02: Personal Message: Store a person’s name in a variable, and print a message to that
+person. Your message should be simple, such as, “Hello Eric, would you like to learn some
+Python today?” */
+
+let name: string = "Tayyaba"
+
+console.log(`Hello! ${name}, Would you like to learn some TypeScript today?`);
