8915d3e6c5d072c26836ce503c83f02729f61baa
Tayyaba Hussain <tayyabahussain1998@gmail.com>
2024-08-09 11:41:11 +0500

    Q9 commit

---
+++ Q09_Favorite_Number/main.js
@@ -0,0 +1,5 @@
+/*Q09: Favorite Number: Store your favorite number in a variable.Then, using that variable,
+create a message that reveals your favorite number. Print that message. */
+let favNum = 4;
+console.log(`My Favorite number is ${favNum}.`);
+export {};
---
+++ Q09_Favorite_Number/main.ts
@@ -0,0 +1,8 @@
+/*Q09: Favorite Number: Store your favorite number in a variable.Then, using that variable,
+create a message that reveals your favorite number. Print that message. */
+
+
+
+let favNum: number = 4
+
+console.log(`My Favorite number is ${favNum}.`);
