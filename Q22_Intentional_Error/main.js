44f7c7dfeeca3593a73aa0de72f10a557ae3c820
Tayyaba Hussain <tayyabahussain1998@gmail.com>
2024-08-09 18:03:59 +0500

    Q22 commit

---
+++ Q22_Intentional_Error/main.js
@@ -0,0 +1,11 @@
+/*Q22: Intentional Error:If you haven’t received an array index error in one of your programs
+yet, try to make one happen. Change an index in one of your programs to produce an index error.
+Make sure you correct the error before closing the program.*/
+//Creating Array
+let newArray = ["Will Smith", "James Bond", "Tom Cruise"];
+//Accessing array by index
+console.log(newArray[5]);
+//Make sure you correct the error before closing the program.
+//Correct the error
+console.log(newArray);
+export {};
