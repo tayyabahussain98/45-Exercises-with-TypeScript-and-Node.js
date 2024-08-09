225bf7edf0279035422f5551fde58157a70d4abd
Tayyaba Hussain <tayyabahussain1998@gmail.com>
2024-08-09 16:35:48 +0500

    Q13 commit

---
+++ Q13_Your_Own_Array/main.js
@@ -0,0 +1,9 @@
+/*Q13: Your Own Array: Think of your favorite mode of transportation,
+such as a motorcycle or a car, and make a list that stores several
+examples.Use your list to print a series of statements about these
+items, such as "I would like to own a Honda motorcycle." */
+let transportation = ["Honda", "Civic", "Corolla", "Lamborghini", "Mercedes"];
+for (let i = 0; i < transportation.length; i++) {
+    console.log(`${transportation[i]} is my favorite transport.`);
+}
+export {};
