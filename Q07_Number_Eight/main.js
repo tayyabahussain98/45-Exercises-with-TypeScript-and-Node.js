b9aafa6825f55d3234e8e60e6391e36c004325f0
Tayyaba Hussain <tayyabahussain1998@gmail.com>
2024-08-09 11:26:38 +0500

    Q7 commit

---
+++ Q07_Number_Eight/main.js
@@ -0,0 +1,8 @@
+/*Q07: Number Eight: Write addition, subtraction, multiplication, and division operations that
+each result in the number 8. Be sure to enclose your operations in print statements to see
+the results.*/
+console.log(5 + 3); // Addition
+console.log(15 - 7); // Subtraction
+console.log(4 * 2); // Multiplication
+console.log(32 / 4); // Division
+export {};
